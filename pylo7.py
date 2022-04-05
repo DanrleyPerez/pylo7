9e75ee7cabc0d8e78da72b064184fa6b1ea4034a
DanrleyPerez <danrley2109@gmail.com>
2022-04-05 11:51:39 -0300

    update

--- pylo7.py
+++ pylo7.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 def indice_palavra_string(arquivo, palavra):                #encontra o índice de uma plavra dentro da página
-    indice = re.search(str(palavra), arquivo)
+    indice = re.search(palavra, arquivo)
     indice = indice.start()
     return indice
 
@@ -30,21 +30,31 @@ def quantidade_de_produtos(termo_de_interesse):
     return int(qte_produtos)
 
 
-def numero_de_paginas(produto):
-    concate = quantidade_de_produtos(produto)
-    numero_paginas = int(concate)/39
-    if int(concate) % 39 == 0:
-        numero_paginas = int(numero_paginas)
-    elif int(concate) < 39:
+def numero_de_paginas(produto, qte_produtos):
+
+    if qte_produtos == 0:
+        qte_produtos = quantidade_de_produtos(produto)
+        if qte_produtos > 2000:
+            qte_produtos = 2000
+
+    else:
+        pass
+
+    if int(qte_produtos) % 39 == 0:
+        numero_paginas = int(qte_produtos / 39)
+
+    elif int(qte_produtos) < 39:
         numero_paginas = 1
     else:
-        numero_paginas = int(numero_paginas + 1)
+        numero_paginas = int(qte_produtos / 39) + 1
     return numero_paginas
+
 
 
 def captura_pagina(termo_interesse, n_pagina):
     req = requests.request('GET', """https://www.elo7.com.br/lista/"""+termo_interesse+"""?pageNum="""+str(n_pagina)+"""""")
     conteudo = str(req.content)
+
     return conteudo
 
 
@@ -64,17 +74,20 @@ def produtos_da_pagina(pagina):                 # função que que determina ond
 
 def todos_produtos(produto_interesse, espaco_amostral):
     todos_produtos = []
-    numero_paginas = numero_de_paginas(produto_interesse)
-    if numero_paginas > 51:                                  # o máximo de paginas do elo 7 é 51.
-        numero_paginas = 51
-    for i in range(0, numero_paginas):
+    todas_paginas = []
+    numero_paginas = numero_de_paginas(produto_interesse, espaco_amostral)
+    if numero_paginas > 50:
+        numero_paginas = 50
+    for i in range(1, numero_paginas + 1):
         print("página:", i)
         conteudo_pagina = captura_pagina(produto_interesse, i)
         produtos_pagina = produtos_da_pagina(conteudo_pagina)
         todos_produtos.append(produtos_pagina)
+        todas_paginas.append(conteudo_pagina)
     produtos_em_lista = tratamento_string(str(todos_produtos), espaco_amostral)
     print(len(produtos_em_lista), " produtos analizados")
-    return produtos_em_lista
+
+    return produtos_em_lista, numero_paginas, todas_paginas[0]
 
 
 def tratamento_string(produtos, espaco_amostral):
@@ -116,6 +129,28 @@ def tratamento_string(produtos, espaco_amostral):
     return list_produtos_organizada
 
 
+def trechos_pagina(lista_trechos_inicio, trecho_parada, pagina):
+    lista_trechos = []
+
+    for trecho in lista_trechos_inicio:
+        indice_inicio_trecho = pagina.find(trecho)
+
+        trecho_total = ""
+        if indice_inicio_trecho == -1:
+            pass
+        else:
+            indice_href_fim = pagina.find(trecho_parada, indice_inicio_trecho + 10)
+            if indice_href_fim == -1:
+                pass
+            else:
+                for l in range(indice_inicio_trecho + 1, indice_href_fim):
+
+                    trecho_total = trecho_total + str(pagina[l])
+
+                lista_trechos.append(trecho_total)
+    return lista_trechos
+
+
 class VendasDeSucesso:
     def __init__(self, termo, espaco_amostral=0):
         """
@@ -127,7 +162,11 @@ class VendasDeSucesso:
         """
         self.termo = termo
         self.espaco_amostral = espaco_amostral
-        self.todos_os_produtos = pd.DataFrame(todos_produtos(self.termo, espaco_amostral))
+        produtos, self.numero_de_paginas, self.primeira_pagina = todos_produtos(self.termo, espaco_amostral)
+        self.todos_os_produtos = pd.DataFrame(produtos)
+
+    def numero_paginas(self):
+        return self.numero_de_paginas
 
     def produtos(self):
         return self.todos_os_produtos
@@ -144,17 +183,63 @@ class VendasDeSucesso:
             qtd_palavras.append(len(palavras))
         return qtd_palavras
 
+    def links_cada_produto(self):
+        """
+
+        :return: Lista com os links das páginas dos 20 primeiros produtos encontrados.
+        """
+        produtos = []
+        hrefs_todos_produtos = []
+        links_produtos = []
+
+        for i in range(20):
+            produtos.append(self.todos_os_produtos['name'][i])
+        pagina = self.primeira_pagina
+
+        for produto in produtos:
+            produto_formatado = produto.replace(" - ", "-").replace(" ", "-").lower()
+            if produto_formatado.find("\\x") == -1:
+                href_na_pagina = "'/" + produto_formatado
+                hrefs_todos_produtos.append(href_na_pagina)
+            else:
+                produto_formatado = produto_formatado.split("\\x", 1)[0]
+                href_na_pagina = "'/"+produto_formatado
+                hrefs_todos_produtos.append(href_na_pagina)
+        links_produtos = trechos_pagina(hrefs_todos_produtos, "'", pagina)
+
+        for i in range(len(links_produtos)):
+            links_produtos[i] = "https://www.elo7.com.br" + links_produtos[i].strip("\\")
+        return links_produtos
+
     def tags_de_sucesso(self):
-        pass
+        links_por_produto = self.links_cada_produto()
+        n=0
+        lista_trechos_com_tags =[]
+
+        for pagina in links_por_produto:
+            n+=1
+            req = requests.request('GET', pagina)
+            pagina = req.content.decode('UTF-8')
+
+            trecho_com_tags = trechos_pagina('<h2>Tag', "</a></li></ol>", pagina)
+
+            lista_trechos_com_tags.append(trecho_com_tags)
+
+
+    def media_primeiros_10(self):
+
+        media_primeiros10 =self.todos_os_produtos[:10].median()
+        print(media_primeiros10)
+        return round(media_primeiros10, 3)
 
     def preco_mediano(self):
-        return self.todos_os_produtos['price'].median()
+        return round(self.todos_os_produtos['price'].median(), 1)
 
     def preco_medio(self):
-        return self.todos_os_produtos['price'].mean()
+        return round(self.todos_os_produtos['price'].mean(), 1)
 
     def info_gerais(self):
-        return self.todos_os_produtos['price'].describe()
+        return round(self.todos_os_produtos['price'].describe(), 1)
 
     def plots(self):
         preco_maximo = self.todos_os_produtos['price'].max()
@@ -174,15 +259,9 @@ def run():
     qte_produtos = input(" Digite a quantidade de Produtos que deseja analizar")
     if qte_produtos == "":
         qte_produtos = 0
-    else:
-        qte_produtos = int(qte_produtos)
-    prod = VendasDeSucesso(termo, qte_produtos)
-    prod.plots()
-    produtos =prod.produtos()
-    informacoes_gerais = prod.info_gerais()
-    print(produtos)
-    print(informacoes_gerais)
 
+    prod = VendasDeSucesso(termo, qte_produtos)
+    print(prod.numero_paginas())
 
 if __name__ == '__main__':
     run()
